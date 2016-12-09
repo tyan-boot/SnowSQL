8b0b9119d890d5c89b22707bbb8a6fdb5be4bbaa
TyanBoot <tyan-boot@outlook.com>
2016-12-09 15:43:46 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+setup(
+    name='SnowSQL',
+    version='0.1.0',
+    description='Python light database framework',
+    author='Tyan Boot',
+    author_email='tyanboot@outlook.com',
+    url='https://github.com/tyan-boot/SnowSQL',
+    packages=['SnowSQL']
+)
+
+setup(
+    name='SnowSQL-Sqlite',
+    version='0.1.0',
+    description='Python light database framework for sqlite',
+    author='Tyan Boot',
+    author_email='tyanboot@outlook.com',
+    url='https://github.com/tyan-boot/SnowSQL',
+    packages=['SnowSQL_Sqlite'],
+    install_requires = [
+        "SnowSQL >= 0.1.0",
+    ]
+)
+
+setup(
+    name='SnowSQL-Mysql',
+    version='0.1.0',
+    description='Python light database framework for mysql',
+    author='Tyan Boot',
+    author_email='tyanboot@outlook.com',
+    url='https://github.com/tyan-boot/SnowSQL',
+    packages=['SnowSQL_Mysql'],
+    install_requires = [
+        "SnowSQL >= 0.1.0",
+        "pymysql"
+    ]
+)
